62cc1e173adc90c527f33b09d8a10788abc4be3b
... <felixutong@gmail.com>
2026-06-29 10:15:00 +0200

    feat(atomic): add compare-and-swap, atomic get/set/add for int, u32, pointer

--- src/atomic.zig
+++ src/atomic.zig
@@ -1,10 +1,15 @@
 // SDL3 Atomic Bindings
 // Lock-free atomic operations
 
+const core = @import("core.zig");
+
 // Atomic types
 pub const SDL_SpinLock = c_int;
 pub const SDL_AtomicInt = extern struct {
     value: c_int,
+};
+pub const SDL_AtomicU32 = extern struct {
+    value: core.Uint32,
 };
 
 // Atomic functions
@@ -15,6 +20,17 @@ extern fn SDL_AtomicIncRef(atomic: ?*anyopaque) c_int;
 extern fn SDL_AtomicDecRef(atomic: ?*anyopaque) c_int;
 extern fn SDL_MemoryBarrierReleaseFunction() void;
 extern fn SDL_MemoryBarrierAcquireFunction() void;
+extern fn SDL_CompareAndSwapAtomicInt(a: ?*SDL_AtomicInt, oldval: c_int, newval: c_int) bool;
+extern fn SDL_SetAtomicInt(a: ?*SDL_AtomicInt, v: c_int) c_int;
+extern fn SDL_GetAtomicInt(a: ?*SDL_AtomicInt) c_int;
+extern fn SDL_AddAtomicInt(a: ?*SDL_AtomicInt, v: c_int) c_int;
+extern fn SDL_CompareAndSwapAtomicU32(a: ?*SDL_AtomicU32, oldval: core.Uint32, newval: core.Uint32) bool;
+extern fn SDL_SetAtomicU32(a: ?*SDL_AtomicU32, v: core.Uint32) core.Uint32;
+extern fn SDL_GetAtomicU32(a: ?*SDL_AtomicU32) core.Uint32;
+extern fn SDL_AddAtomicU32(a: ?*SDL_AtomicU32, v: c_int) core.Uint32;
+extern fn SDL_CompareAndSwapAtomicPointer(a: ?*?*anyopaque, oldval: ?*anyopaque, newval: ?*anyopaque) bool;
+extern fn SDL_SetAtomicPointer(a: ?*?*anyopaque, v: ?*anyopaque) ?*anyopaque;
+extern fn SDL_GetAtomicPointer(a: ?*?*anyopaque) ?*anyopaque;
 
 // Public API
 pub const tryLockSpinlock = SDL_TryLockSpinlock;
@@ -24,3 +40,14 @@ pub const atomicIncRef = SDL_AtomicIncRef;
 pub const atomicDecRef = SDL_AtomicDecRef;
 pub const memoryBarrierReleaseFunction = SDL_MemoryBarrierReleaseFunction;
 pub const memoryBarrierAcquireFunction = SDL_MemoryBarrierAcquireFunction;
+pub const compareAndSwapAtomicInt = SDL_CompareAndSwapAtomicInt;
+pub const setAtomicInt = SDL_SetAtomicInt;
+pub const getAtomicInt = SDL_GetAtomicInt;
+pub const addAtomicInt = SDL_AddAtomicInt;
+pub const compareAndSwapAtomicU32 = SDL_CompareAndSwapAtomicU32;
+pub const setAtomicU32 = SDL_SetAtomicU32;
+pub const getAtomicU32 = SDL_GetAtomicU32;
+pub const addAtomicU32 = SDL_AddAtomicU32;
+pub const compareAndSwapAtomicPointer = SDL_CompareAndSwapAtomicPointer;
+pub const setAtomicPointer = SDL_SetAtomicPointer;
+pub const getAtomicPointer = SDL_GetAtomicPointer;
